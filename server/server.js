47d53aa7244f12314bc9082a5e9a673f26d580e2
Moshe Wahba <moshewahba@Eliks-MacBook-Air.local>
2018-05-13 11:18:55 +0300

    heruko

--- server/server.js
+++ server/server.js
@@ -9,6 +9,7 @@ var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
 
 var app = express();
+const port = process.env.PORT || 3001;
 
 app.use(bodyParser.json());
 
@@ -50,8 +51,8 @@ app.get('/todos/:id',(req,res)=>{
 
 })
 
-app.listen(3001, () => {
-    console.log('start port 3001');
+app.listen(port, () => {
+    console.log(`start port 3001 ${port}`);
 });
 
 module.exports = {app};
